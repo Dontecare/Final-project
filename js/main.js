048fb71d7cbe2c315045f289079d8a1f5b0e4ee9
Dontecare <Dontecare1@gmail.com>
2023-11-28 11:22:50 -0600

    Added incomplete new home page layout

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// JS scripts placed here
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+    }
+}
+// swap themes
+// function toggleActive(element) {
+//     element.closest('.site-main-wrapper').classList.toggle('active');
+// }
